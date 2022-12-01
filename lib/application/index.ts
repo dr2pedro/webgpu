900b93a639fc1b6d9b827aef61b2eaf9bb629d42
dr2pedro <dr2p@hotmail.com>
2022-12-01 11:59:03 -0300

    corrigindo typo

---
+++ lib/application/index.ts
@@ -0,0 +1,3 @@
+export default class MatrixManipulation {
+    constructor()
+}
--- lib/infra/test.ts
+++ lib/infra/test.ts
@@ -8,53 +8,38 @@ const repository = new GPUCommandRepository();
 const adapter = await GPUDeviceAdapter.init(repository)
 
 if(adapter.adapter !== null) {
-    const device =  adapter.device;
     const firstMatrix = new Float32Array([
-        2, 4,
-        1, 2, 3, 4,
-        5, 6, 7, 8
+        3, 5,
+        1, 2, 3, 4, 11,
+        5, 6, 7, 8, 15,
+        21, 40, 55, 18
       ]);
-    const { buffer: gpuBufferFirstMatrix, layout: layoutFirstMatrix } = new Buffer(adapter, GPUBufferBindingType["read-only-storage"]).create(firstMatrix.byteLength, GPUBufferUsage.STORAGE, true, 'firstMatrix');
-    const arrayBufferFirstMatrix = gpuBufferFirstMatrix.getMappedRange();
+    const firstMatrixBuffer = new Buffer(adapter, GPUBufferBindingType["read-only-storage"]).create(firstMatrix.byteLength, GPUBufferUsage.STORAGE, true, 'firstMatrix');
+    const arrayBufferFirstMatrix = firstMatrixBuffer.buffer.getMappedRange();
     new Float32Array(arrayBufferFirstMatrix).set(firstMatrix);
-    gpuBufferFirstMatrix.unmap();
+    firstMatrixBuffer.buffer.unmap();
       
     const secondMatrix = new Float32Array([
-        4, 2,
-        1, 2,
-        3, 4,
-        5, 6,
-        7, 8
+        5, 3,
+        1, 2, 55,
+        3, 4, 44,
+        5, 6, 106,
+        7, 8, 200,
+        9, 10, 66
     ]);
     
-    const { buffer: gpuBufferSecondMatrix, layout: layoutSecondMatrix } = new Buffer(adapter, GPUBufferBindingType["read-only-storage"]).create(secondMatrix.byteLength, GPUBufferUsage.STORAGE, true, 'secondMatrix');
-    const arrayBufferSecondMatrix = gpuBufferSecondMatrix.getMappedRange();
+    const secondMatrixBuffer = new Buffer(adapter, GPUBufferBindingType["read-only-storage"]).create(secondMatrix.byteLength, GPUBufferUsage.STORAGE, true, 'secondMatrix');
+    const arrayBufferSecondMatrix = secondMatrixBuffer.buffer.getMappedRange();
     new Float32Array(arrayBufferSecondMatrix).set(secondMatrix);
-    gpuBufferSecondMatrix.unmap();
+    secondMatrixBuffer.buffer.unmap();
 
     const resultMatrixBufferSize = Float32Array.BYTES_PER_ELEMENT * (2 + firstMatrix[0] * secondMatrix[1]);
-    const { buffer: resultMatrixBuffer, layout: layoutResultMatrix } = new Buffer(adapter, GPUBufferBindingType.storage).create(resultMatrixBufferSize, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, false, 'resultMatrix');
-    const { layout: bindGroupLayout, bind: bindGroup } = new Group(
-        adapter, 
-        [ 
-            { 
-                buffer: gpuBufferFirstMatrix, 
-                layout: layoutFirstMatrix 
-            },
-            {
-                buffer: gpuBufferSecondMatrix,
-                layout: layoutSecondMatrix
-            },
-            {
-                buffer: resultMatrixBuffer,
-                layout: layoutResultMatrix
-            }
-        ]).create()
-    const shaderModule = device.createShaderModule({ code:  shader });
+    const resultMatrixBuffer = new Buffer(adapter, GPUBufferBindingType.storage).create(resultMatrixBufferSize, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, false, 'resultMatrix');
+    const { layout: bindGroupLayout, bind: bindGroup } = new Group(adapter, [ firstMatrixBuffer, secondMatrixBuffer, resultMatrixBuffer]).create()
+    const shaderModule = adapter.device.createShaderModule({ code:  shader });
     const computePipeline = new Pipeline(adapter, [bindGroupLayout], shaderModule, "main").create();
-    //const gpuReadBuffer = device.createBuffer({ size: resultMatrixBufferSize, usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ });
     const { buffer: gpuReadBuffer } = new Buffer(adapter).create(resultMatrixBufferSize, GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ)
-    const commandEncoder = new Command(adapter).pipeline(computePipeline).group(bindGroup, 0).size({ x: Math.ceil(firstMatrix[0] / 8), y:Math.ceil(secondMatrix[1] / 8) }).bindArrays(resultMatrixBuffer,gpuReadBuffer,resultMatrixBufferSize) .end()
+    const commandEncoder = new Command(adapter).pipeline(computePipeline).group(bindGroup, 0).size({ x: Math.ceil(firstMatrix[0] / 8), y:Math.ceil(secondMatrix[1] / 8) }).bindArrays(resultMatrixBuffer.buffer,gpuReadBuffer, resultMatrixBufferSize) .end()
     
     repository.push(commandEncoder);
     adapter.run();
